3ac540f2bc3b313cc9dbaf0ca76924dcd168dbcb
ayushman sikder <ayushmansikder.ai@gmail.com>
2026-02-07 23:58:30 +0530

    explain

--- 0_day0.py
+++ 0_day0.py
@@ -2,6 +2,5 @@ print("hello DSA")
 print("i will do dsa for 21days regularly")
 print("just learned time complexcity and space complexcity")
 
-for i in range (1,6):
+for i in range (1,6):  # (i = 1 , i < 6 , i++)
     print("ayush")
-    
