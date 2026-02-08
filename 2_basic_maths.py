3de04daacd6bc7370b3c4e51867773e13cb3f34d
ayushman sikder <ayushmansikder.ai@gmail.com>
2026-02-08 13:31:01 +0530

    check palindrome

--- 2_basic_maths.py
+++ 2_basic_maths.py
@@ -15,3 +15,15 @@ while num>0:
     count += 1
     num = num // 10
 print(f"the digit countof the number {n} is {count}")
+
+
+# palindrome number
+n = 12321
+num = n
+result = 0
+while num > 0:
+    id = num % 10
+    result = (result *10)+id
+    num = num//10 
+print(f"{n} is a palindrome? ==> {n == result}")
+
