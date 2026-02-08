15defac0d53e26dae342a830e7d16b264e7743bb
ayushman sikder <ayushmansikder.ai@gmail.com>
2026-02-08 14:13:18 +0530

    time and space complexcity

--- 2_basic_maths.py
+++ 2_basic_maths.py
@@ -40,4 +40,5 @@ while num>0:
     total = total +(last_digit**no_of_digit)
     num = num // 10
 print(f"is {n} is a armstrong number? ==> {total == n}")
+#  time complexity ==> O(log10(N)) space complexity ==> O(1)
 
