11f373e7f4055fa01ebeea470745de40822559d8
ayushman sikder <ayushmansikder.ai@gmail.com>
2026-02-08 13:27:23 +0530

    counting digits

--- 2_basic_maths.py
+++ 2_basic_maths.py
@@ -7,4 +7,11 @@ while num>0:
     print(last)
     num = num //10
 
-
+# counting digits
+n = 5874
+num = n
+count = 0
+while num>0:
+    count += 1
+    num = num // 10
+print(f"the digit countof the number {n} is {count}")
