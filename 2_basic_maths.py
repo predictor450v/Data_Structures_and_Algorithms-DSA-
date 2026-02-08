0f21f900546a2dce6664ad18d0154a0fee879044
ayushman sikder <ayushmansikder.ai@gmail.com>
2026-02-08 13:33:23 +0530

    time and space complexity

--- 2_basic_maths.py
+++ 2_basic_maths.py
@@ -6,6 +6,7 @@ while num>0:
     last = num%10
     print(last)
     num = num //10
+#  time complexity ==> O(log10(N)) space complexity ==> O(1)
 
 # counting digits
 n = 5874
@@ -15,7 +16,7 @@ while num>0:
     count += 1
     num = num // 10
 print(f"the digit countof the number {n} is {count}")
-
+#  time complexity ==> O(log10(N)) space complexity ==> O(1)
 
 # palindrome number
 n = 12321
@@ -26,4 +27,4 @@ while num > 0:
     result = (result *10)+id
     num = num//10 
 print(f"{n} is a palindrome? ==> {n == result}")
-
+#  time complexity ==> O(log10(N)) space complexity ==> O(1)
