162dfef80b673f762eacfa5d65e09923dbab2888
ayushman sikder <ayushmansikder.ai@gmail.com>
2026-02-08 14:12:37 +0530

    armstrong number

--- 2_basic_maths.py
+++ 2_basic_maths.py
@@ -28,3 +28,16 @@ while num > 0:
     num = num//10 
 print(f"{n} is a palindrome? ==> {n == result}")
 #  time complexity ==> O(log10(N)) space complexity ==> O(1)
+
+# check Armstrong number
+n = 153
+num = n 
+total = 0 
+no_of_digit = len(str(num))
+total = 0
+while num>0:
+    last_digit = num % 10
+    total = total +(last_digit**no_of_digit)
+    num = num // 10
+print(f"is {n} is a armstrong number? ==> {total == n}")
+
