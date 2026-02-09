e1df086a70048ec5972f9ce69168731ba60bc208
ayushman sikder <ayushmansikder.ai@gmail.com>
2026-02-10 00:31:53 +0530

    using dictioary checking how mane elemnts  of list 2 present in list 1

---
+++ 3_Hashing/2_hashDict.py
@@ -0,0 +1,13 @@
+n = [5,3,2,4,1,5,5,7,5,10,2,1,3]
+m = [10,111,1,9,5,6,67,2,4]
+num = len(n)
+hash_dict = {}
+for i in range (0,num):
+    hash_dict[n[i]] = hash_dict.get(n[i],0)+1
+print(hash_dict)
+
+for e in m:
+    if e < 1 or e > 10: 
+        print(0)
+    else:
+        print(hash_dict.get(e,0))
