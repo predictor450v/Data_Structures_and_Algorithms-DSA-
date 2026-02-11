6ebec1434aea870017669b3495f7f597b1e2338c
ayushman sikder <ayushmansikder.ai@gmail.com>
2026-02-11 23:51:02 +0530

    comments

--- 4_recursion/1_head_recursion.py
+++ 4_recursion/1_head_recursion.py
@@ -1,9 +1,11 @@
 count = 0 
 def func():
-    global count
+    # global count   declairing count a as global varibal so that local count will be considered as global
     if count == 4:
         return
+    # first print and logic
     print("ayush")
     count += 1
+    # recurtion call at last
     func()
 func()
