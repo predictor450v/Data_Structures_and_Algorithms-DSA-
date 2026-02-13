79045cea860986c1394f4baccc228e7a68373f35
ayushman sikder <ayushmansikder.ai@gmail.com>
2026-02-14 00:58:24 +0530

    changed

--- 4_recursion/6_factorial.py
+++ 4_recursion/6_factorial.py
@@ -1,6 +1,6 @@
 def fact(n):
-    if n == 0:
+    if n == 0 or n == 1:
         return 1 
     return n * fact(n-1)
 print(fact(1))
-# time complexity is O(n) and space complexity is O(n) because of recursive stack
+#time complexity is O(n) and space complexity is O(n) because of recursive stack
