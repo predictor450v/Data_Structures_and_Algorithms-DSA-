13d70b01da410015af9d788fd13a0935d2ccb931
ayushman sikder <ayushmansikder.ai@gmail.com>
2026-02-16 21:49:55 +0530

    reverse array

---
+++ 4_recursion/7_array_reverse_using recursion.py
@@ -0,0 +1,13 @@
+# reverser array using recursion
+array = [1, 2, 3, 4, 5,6,7,8,9,10]
+def reverse_array(arr, start, end):
+    if start >= end:
+        return
+    # swap the elements at start and end
+    arr[start], arr[end] = arr[end], arr[start]
+    # recursive call for the remaining array
+    reverse_array(arr, start + 1, end - 1)
+# example usage
+
+reverse_array(array,4,7)
+print(array)  
