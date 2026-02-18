6fc17dd253e2f29941135aa6b663c559af2e3a36
ayushman sikder <ayushmansikder.ai@gmail.com>
2026-02-18 20:22:24 +0530

    some about linked list

--- 6_linked_list/1_linked_list.py
+++ 6_linked_list/1_linked_list.py
@@ -1,1 +1,15 @@
-print("hello world")
+#  what is linked list
+#  linked list is a linear data structure where each element is a separate object
+#  each element (node) of a linked list contains two parts: data and a reference (or link) to the next node in the sequence
+#  linked list is a dynamic data structure which can grow and shrink at runtime by allocating and deallocating memory as needed
+#  linked list can be used to implement other data structures like stacks, queues, and graphs
+#  advantages of linked list over array
+# 1. dynamic size: linked list can grow and shrink at runtime, while array has
+# a fixed size which must be defined at the time of declaration
+# 2. ease of insertion/deletion: linked list allows for easy insertion and deletion of
+# elements without the need to shift elements as in an array
+# 3. memory utilization: linked list can utilize memory more efficiently than an array, as
+# it does not require contiguous memory allocation like an array
+# 4. implementation of other data structures: linked list can be used to implement other data
+# structures like stacks, queues, and graphs, while arrays may not be as efficient for these purposes
+
